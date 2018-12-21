a57d78f37a51dd59c2974d787696fe66af673a2e
myoneday <467347692@qq.com>
2018-12-21 17:56:50 +0800

    规范接口输出和bug修改

--- controller/GoodsController.java
+++ controller/GoodsController.java
@@ -1,12 +1,21 @@
-package com.example.taoxiao;
+package com.example.taoxiao.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.taoxiao.entity.rentgoods;
+import com.example.taoxiao.entity.sellgoods;
+import com.example.taoxiao.mapper.GoodsMapper;
+import com.example.taoxiao.mapper.goodsImageMapper;
+
+import com.example.taoxiao.FileUtils;
 
 //设置访问的接口的文件
 
@@ -17,64 +26,55 @@ public class GoodsController {
 		@Autowired
 		private GoodsMapper goodsm;
 		
+		
 		@RequestMapping("/newgoods")
-		public String addgoods(String goodsname,String sellid, String kinds,double price, String description, Date uptime){
+		public String addgoods(String goodsname,String sellid, String type,double price, String description, Date uptime){
 			JSONObject result = new JSONObject();
-			if( goodsm.insert(goodsname,sellid,kinds,price,description,uptime))
+			if( goodsm.insert(goodsname,sellid,type,price,description,uptime))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
 			return result.toJSONString();
 		}
 		@RequestMapping("/getgoodsINF")
-		public String select2(String id){//根据商品ID查找商品
-			JSONObject result = new JSONObject();
-			List<sellgoods> str=goodsm.select2(id);
-			result.put("sreach",str);
-			return result.toJSONString();
-
+		public List<sellgoods> select2(int id){//根据商品ID查找商品
+			return goodsm.select2(id);
 		}
 		@RequestMapping("/getgoodsINFBytype")//根据类型返回商品信息
-		public String select3(String type){
-
-			JSONObject result = new JSONObject();
-			List<sellgoods> str=goodsm.select3(type);
-			result.put("sreach",str);
-			return result.toJSONString();
+		public List<sellgoods> select3(String type){
+			return goodsm.select3(type);
+		}
+		@RequestMapping("/getgoodsINFsellid")
+		public List<sellgoods> select4(String sellid){//根据用户userid查看自己销售物品
+			return goodsm.select4(sellid);
 		}
 		@RequestMapping("/delete")//删除商品信息
-			public String delete(String id){
+			public String delete(int id){
 			JSONObject result = new JSONObject();
-			if( goodsm.delete(id))
+			if( goodsm.delete(id)&& FileUtils.delete(id))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
+			// && FileUtils.delete(id)
 			return result.toJSONString();
 		}
 		@RequestMapping("/update")//更新商品信息
-				public String update(String goodsid,String goodsname,String sellid,String kinds,double price,String description,Date uptime){
+		public String update(String goodsid,String goodsname,String sellid,String type,double price,String description,Date uptime){
 			JSONObject result = new JSONObject();
-			if( goodsm.update(goodsid,goodsname,sellid,kinds,price,description,uptime))
+			if( goodsm.update(goodsid,goodsname,sellid,type,price,description,uptime))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
 			return result.toJSONString();
 		}
 		@RequestMapping("/Sreachsell")//搜索买卖的商品
-				public String Searchsell(String Text)
+		public List<sellgoods> Searchsell(String Text)
 		{
-			JSONObject result = new JSONObject();
-			List<sellgoods> str=goodsm.Searchsell(Text);
-			result.put("sreach",str);
-			return result.toJSONString();
+			return goodsm.Searchsell(Text);
 		}
-	@RequestMapping("/Sreachrent")//搜索租借的商品
-	public String Searchrent(String Text)
-	{
-		JSONObject result = new JSONObject();
-		List<rentgoods> str=goodsm.Searchrent(Text);
-		result.put("sreach",str);
-		return result.toJSONString();
-	}
+		@RequestMapping("/Sreachrent")//搜索租借的商品
+		public List<rentgoods> Searchrent(String Text){
+			return goodsm.Searchrent(Text);
+		}
 
 }
