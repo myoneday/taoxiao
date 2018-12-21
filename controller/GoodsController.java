c2c663276da8debcb14332ebf2900f38236488d2
myoneday <467347692@qq.com>
2018-12-21 19:04:58 +0800

    Revert "规范接口输出和bug修改"
    This reverts commit a57d78f37a51dd59c2974d787696fe66af673a2e.

--- controller/GoodsController.java
+++ controller/GoodsController.java
@@ -1,21 +1,12 @@
-package com.example.taoxiao.controller;
+package com.example.taoxiao;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.alibaba.fastjson.JSONObject;
-import com.example.taoxiao.entity.rentgoods;
-import com.example.taoxiao.entity.sellgoods;
-import com.example.taoxiao.mapper.GoodsMapper;
-import com.example.taoxiao.mapper.goodsImageMapper;
-
-import com.example.taoxiao.FileUtils;
 
 //设置访问的接口的文件
 
@@ -26,55 +17,64 @@ public class GoodsController {
 		@Autowired
 		private GoodsMapper goodsm;
 		
-		
 		@RequestMapping("/newgoods")
-		public String addgoods(String goodsname,String sellid, String type,double price, String description, Date uptime){
+		public String addgoods(String goodsname,String sellid, String kinds,double price, String description, Date uptime){
 			JSONObject result = new JSONObject();
-			if( goodsm.insert(goodsname,sellid,type,price,description,uptime))
+			if( goodsm.insert(goodsname,sellid,kinds,price,description,uptime))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
 			return result.toJSONString();
 		}
 		@RequestMapping("/getgoodsINF")
-		public List<sellgoods> select2(int id){//根据商品ID查找商品
-			return goodsm.select2(id);
+		public String select2(String id){//根据商品ID查找商品
+			JSONObject result = new JSONObject();
+			List<sellgoods> str=goodsm.select2(id);
+			result.put("sreach",str);
+			return result.toJSONString();
+
 		}
 		@RequestMapping("/getgoodsINFBytype")//根据类型返回商品信息
-		public List<sellgoods> select3(String type){
-			return goodsm.select3(type);
-		}
-		@RequestMapping("/getgoodsINFsellid")
-		public List<sellgoods> select4(String sellid){//根据用户userid查看自己销售物品
-			return goodsm.select4(sellid);
+		public String select3(String type){
+
+			JSONObject result = new JSONObject();
+			List<sellgoods> str=goodsm.select3(type);
+			result.put("sreach",str);
+			return result.toJSONString();
 		}
 		@RequestMapping("/delete")//删除商品信息
-			public String delete(int id){
+			public String delete(String id){
 			JSONObject result = new JSONObject();
-			if( goodsm.delete(id)&& FileUtils.delete(id))
+			if( goodsm.delete(id))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
-			// && FileUtils.delete(id)
 			return result.toJSONString();
 		}
 		@RequestMapping("/update")//更新商品信息
-		public String update(String goodsid,String goodsname,String sellid,String type,double price,String description,Date uptime){
+				public String update(String goodsid,String goodsname,String sellid,String kinds,double price,String description,Date uptime){
 			JSONObject result = new JSONObject();
-			if( goodsm.update(goodsid,goodsname,sellid,type,price,description,uptime))
+			if( goodsm.update(goodsid,goodsname,sellid,kinds,price,description,uptime))
 				result.put("status", "true");
 			else
 				result.put("status", "false");
 			return result.toJSONString();
 		}
 		@RequestMapping("/Sreachsell")//搜索买卖的商品
-		public List<sellgoods> Searchsell(String Text)
+				public String Searchsell(String Text)
 		{
-			return goodsm.Searchsell(Text);
+			JSONObject result = new JSONObject();
+			List<sellgoods> str=goodsm.Searchsell(Text);
+			result.put("sreach",str);
+			return result.toJSONString();
 		}
-		@RequestMapping("/Sreachrent")//搜索租借的商品
-		public List<rentgoods> Searchrent(String Text){
-			return goodsm.Searchrent(Text);
-		}
+	@RequestMapping("/Sreachrent")//搜索租借的商品
+	public String Searchrent(String Text)
+	{
+		JSONObject result = new JSONObject();
+		List<rentgoods> str=goodsm.Searchrent(Text);
+		result.put("sreach",str);
+		return result.toJSONString();
+	}
 
 }
